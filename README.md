a1251badb976c7c80e1219dbf851e6f99b3e2b1b
Ben Cook <bencook@skiomusic.com>
2016-09-30 12:41:10 -0700

    Add readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# jqllib
+
+Lightweight client for making Mixpanel JQL queries.
+
+This is an alternative to including the heavyweight official `MP` library.
+
+## Usage
+
+```javascript
+import jqllib from 'jqllib'
+
+jqllib.setApiKey(MIXPANEL_SECRET_KEY)
+
+const query = jqllib.baseJql(['Account Created'], ['2016-07-01', '2016-07-08'])
+
+jqllib
+    .mpFetch(query)
+    .then(events => { console.log(events) })
+
+// [{name: 'Account Created', distinct_id: 1234, properties: {...}, ...}]
+
+const groupedByUserQuery = jqllib.groupedJql(['Clicked Button A', 'Clicked Button B'],
+                                             ['2016-09-01', '2016-09-15'])
+
+jqllib
+    .mpFetch(groupedByUserQuery)
+    .then(groups => {
+        groups.forEach(({key, events}) => {
+            console.log(`User ${key} clicked Buttons A/B ${events.length} times`)
+        })
+    })
+
+// User 9101 clicked Buttons A/B 3 times
+// User 9505 clicked Buttons A/B 5 times
+// ...
+```
--- index.js
+++ index.js
@@ -7,11 +7,25 @@ const MP_API_JQL_URI = 'https://mixpanel.com/api/2.0/jql/'
 let _headers = new Headers()
 _headers.set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
 
+/**
+ * Sets the Mixpanel secret key to use for requests.
+ * Note: jqllib is stateful; this key is shared across all usages.
+ */
+// str -> ()
 export function setApiKey(key) {
     _headers.set('Authorization', `Basic ${btoa(key + ':')}`)
 }
 
-export function mpFetch(jql) {
+/**
+ * Returns the results of running the `jql` at Mixpanel (either a list of events or a list of groups,
+ * depending on your JQL).
+ * Note: the Mixpanel API limits running time to ~2 mins.
+ */
+// <MPResult> str -> Promise<[MPResult]>
+// where MPResult :: MPEvent | MPGroup
+// where MPEvent :: {name: str, distinct_id: str, time: int, sampling_factor: int, properties: {[str]: str}}
+//       MPGroup :: {key: [str], value: [<WHATEVER>]}
+function mpFetch(jql) {
     if (!_headers.has('Authorization')) { throw 'API key not set' }
 
     jql = `function main() { ${jql} }`
@@ -29,18 +43,15 @@ export function mpFetch(jql) {
     })
     .then(r => r.json())
 }
+export const fetch = mpFetch
 
 /**
  * JQL query to fetch the `events` within `dateRange`, grouped by distinct user.
  */
-export function baseJql(events, dateRange, propsExpr='x.properties') {
-    const [from_date, to_date] = dateRange.map(s => dateFormat(moment(s)))
+// [EventName], (Momentable, Momentable), str? -> str
+export function groupedJql(events, dateRange, propsExpr='x.properties') {
     return `
-        return Events(${JSON.stringify({
-            from_date,
-            to_date,
-            event_selectors: events
-        })})
+        ${baseJql(events, dateRange)}
         .groupByUser((acc, xs) => {
             acc = acc || {events: []}
             acc.events = acc.events.concat(xs.map(x => ({
@@ -50,6 +61,23 @@ export function baseJql(events, dateRange, propsExpr='x.properties') {
             })))
             return acc
         })
+    `
+}
+
+/**
+ * JQL query to fetch the `events` within `dateRange`.
+ *
+ * Note: JQL runs in a limited environment. Underscore.js is available.
+ */
+// [EventName], (Momentable, Momentable) -> str
+export function baseJql(events, dateRange) {
+    const [from_date, to_date] = dateRange.map(s => dateFormat(moment(s)))
+    return `
+        return Events(${JSON.stringify({
+            from_date,
+            to_date,
+            event_selectors: events
+        })})
     `
 }
 
